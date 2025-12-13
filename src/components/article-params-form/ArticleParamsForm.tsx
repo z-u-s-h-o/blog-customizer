99d804fb1571786b85a31acf305cb1323d313669
z-u-s-h-o <zigy0ygiz@yandex.ru>
2025-12-13 13:19:20 +0500

    feat: implement sidebar and component data exchange logic

--- src/components/article-params-form/ArticleParamsForm.tsx
+++ src/components/article-params-form/ArticleParamsForm.tsx
@@ -1,16 +1,162 @@
+import { useState, useEffect, useRef } from 'react';
 import { ArrowButton } from 'src/ui/arrow-button';
+import { Select } from 'src/ui/select';
 import { Button } from 'src/ui/button';
+import { RadioGroup } from 'src/ui/radio-group';
+import { Separator } from 'src/ui/separator';
+import { Text } from 'src/ui/text';
+import {
+	fontFamilyOptions,
+	fontSizeOptions,
+	fontColors,
+	backgroundColors,
+	contentWidthArr,
+	OptionType,
+	ArticleStateType,
+} from 'src/constants/articleProps';
 
+import clsx from 'clsx';
 import styles from './ArticleParamsForm.module.scss';
 
-export const ArticleParamsForm = () => {
+type ArticleParamsFormProps = {
+	articlePageState: ArticleStateType;
+	onStateChange: (updates: Partial<ArticleStateType>) => void;
+	onReset: () => void;
+};
+
+export const ArticleParamsForm = (props: ArticleParamsFormProps) => {
+	const { articlePageState, onStateChange, onReset } = props;
+
+	//Состояние сайдбара
+	const [isOpen, setIsOpen] = useState(false);
+	const sidebarRef = useRef<HTMLDivElement>(null);
+
+	//Локальное состояние для выбранных параметров (не влияет на articlePageState до нажатия "Применить")
+	const [articleFormState, setArticleFormState] =
+		useState<ArticleStateType>(articlePageState);
+
+	//Меняем состояние сайдбара при нажатии на «стрелку»
+	const handleToggle = () => {
+		setIsOpen(!isOpen);
+	};
+
+	// Обеспечиваем обработку клика вне элемента сайдбара для его скрытия
+	// + синхронизируем состояние формы статьи с текущими, примененными настройками
+	useEffect(() => {
+		if (!isOpen) return;
+
+		const handleClickOutside = (event: MouseEvent) => {
+			if (
+				sidebarRef.current &&
+				!sidebarRef.current.contains(event.target as Node)
+			) {
+				setIsOpen(false);
+			}
+		};
+
+		document.addEventListener('mousedown', handleClickOutside);
+		setArticleFormState(articlePageState);
+
+		return () => document.removeEventListener('mousedown', handleClickOutside);
+	}, [isOpen]);
+
+	// Обновляем локальное состояние при выборе опций
+	const handleOptionChange = (
+		key: keyof ArticleStateType,
+		selected: OptionType
+	) => {
+		setArticleFormState((prev) => ({ ...prev, [key]: selected }));
+	};
+
+	// При нажатии "Применить" передаём все изменения для страницы статьи, закрываем сайдбар
+	const handleApply = (e: React.FormEvent) => {
+		e.preventDefault();
+
+		const changedFields: Partial<ArticleStateType> = {};
+
+		Object.keys(articleFormState).forEach((key) => {
+			const k = key as keyof ArticleStateType;
+			if (
+				articlePageState[k]?.value !== articleFormState[k]?.value &&
+				k in articlePageState
+			) {
+				changedFields[k] = articleFormState[k];
+			}
+		});
+
+		onStateChange(changedFields);
+		setIsOpen(false);
+	};
+
+	// При нажатии "Сбросить", выполняем переданную функцию и закрываем сайдбар
+	const handleReset = () => {
+		onReset();
+		setIsOpen(false);
+	};
+
 	return (
 		<>
-			<ArrowButton isOpen={false} onClick={() => {}} />
-			<aside className={styles.container}>
-				<form className={styles.form}>
+			<ArrowButton isOpen={isOpen} onClick={handleToggle} />
+			<aside
+				ref={sidebarRef}
+				className={clsx(styles.container, { [styles.container_open]: isOpen })}>
+				<form className={styles.form} onSubmit={handleApply}>
+					<div className={styles.optionContainer}>
+						<Text size={31} weight={800} uppercase>
+							Задайте параметры
+						</Text>
+						<Select
+							selected={articleFormState.fontFamilyOption}
+							options={fontFamilyOptions}
+							placeholder='Выберите шрифт'
+							onChange={(selected) =>
+								handleOptionChange('fontFamilyOption', selected)
+							}
+							title='Шрифт'
+						/>
+						<RadioGroup
+							name='string'
+							options={fontSizeOptions}
+							selected={articleFormState.fontSizeOption}
+							onChange={(selected) =>
+								handleOptionChange('fontSizeOption', selected)
+							}
+							title='Font_size'
+						/>
+						<Select
+							selected={articleFormState.fontColor}
+							options={fontColors}
+							placeholder='Выберите цвет шрифта'
+							onChange={(selected) => handleOptionChange('fontColor', selected)}
+							title='Цвет шрифта'
+						/>
+						<Separator />
+						<Select
+							selected={articleFormState.backgroundColor}
+							options={backgroundColors}
+							placeholder='Выберите цвет фона'
+							onChange={(selected) =>
+								handleOptionChange('backgroundColor', selected)
+							}
+							title='Цвет фона'
+						/>
+						<Select
+							selected={articleFormState.contentWidth}
+							options={contentWidthArr}
+							placeholder='Выберите ширину контента'
+							onChange={(selected) =>
+								handleOptionChange('contentWidth', selected)
+							}
+							title='Ширина контента'
+						/>
+					</div>
 					<div className={styles.bottomContainer}>
-						<Button title='Сбросить' htmlType='reset' type='clear' />
+						<Button
+							title='Сбросить'
+							htmlType='reset'
+							type='clear'
+							onClick={handleReset}
+						/>
 						<Button title='Применить' htmlType='submit' type='apply' />
 					</div>
 				</form>
--- src/index.tsx
+++ src/index.tsx
@@ -1,31 +1,55 @@
+import { useState, StrictMode, CSSProperties } from 'react';
 import { createRoot } from 'react-dom/client';
-import { StrictMode, CSSProperties } from 'react';
-import clsx from 'clsx';
 
 import { Article } from './components/article/Article';
 import { ArticleParamsForm } from './components/article-params-form/ArticleParamsForm';
-import { defaultArticleState } from './constants/articleProps';
+import {
+	defaultArticleState,
+	ArticleStateType,
+} from './constants/articleProps';
 
 import './styles/index.scss';
 import styles from './styles/index.module.scss';
+import clsx from 'clsx';
 
 const domNode = document.getElementById('root') as HTMLDivElement;
 const root = createRoot(domNode);
 
 const App = () => {
+	// Состояние для страницы
+	const [articlePageState, setArticlePageState] =
+		useState<ArticleStateType>(defaultArticleState);
+
+	// Обработчик изменения состояния страницы при применении изменений в форме
+	const handleStateArticlePageChange = (updates: Partial<ArticleStateType>) => {
+		setArticlePageState((prev) => ({
+			...prev,
+			...updates,
+		}));
+	};
+
+	// Сброс к дефолтным настройкам
+	const handleReset = () => {
+		setArticlePageState(defaultArticleState);
+	};
+
 	return (
 		<main
 			className={clsx(styles.main)}
 			style={
 				{
-					'--font-family': defaultArticleState.fontFamilyOption.value,
-					'--font-size': defaultArticleState.fontSizeOption.value,
-					'--font-color': defaultArticleState.fontColor.value,
-					'--container-width': defaultArticleState.contentWidth.value,
-					'--bg-color': defaultArticleState.backgroundColor.value,
+					'--font-family': articlePageState.fontFamilyOption.value,
+					'--font-size': articlePageState.fontSizeOption.value,
+					'--font-color': articlePageState.fontColor.value,
+					'--container-width': articlePageState.contentWidth.value,
+					'--bg-color': articlePageState.backgroundColor.value,
 				} as CSSProperties
 			}>
-			<ArticleParamsForm />
+			<ArticleParamsForm
+				articlePageState={articlePageState}
+				onStateChange={handleStateArticlePageChange}
+				onReset={handleReset}
+			/>
 			<Article />
 		</main>
 	);
